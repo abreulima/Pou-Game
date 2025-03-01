9ffb85a0bd38f203fcb17509ef07767867920177
Ivan <34632454+abreulima@users.noreply.github.com>
2025-03-01 10:15:00 +0000

    Add files via upload

---
+++ class_poo.py
@@ -0,0 +1,25 @@
+class Poo:
+    nome = ""
+    fome = 5
+    felicidade = 5
+
+    def __init__(self, name):
+        self.nome = name
+        self.fome = 0
+        self.felicidade = 5
+
+    def alimentar(self):
+        self.fome -= 1
+        if self.fome < 0:
+            self.fome = 0
+
+    def brincar(self):
+        self.felicidade += 1
+        if self.felicidade > 10:
+            self.felicidade = 10
+
+    def mostrar_status(self):
+        print(f"Nome {self.nome}")
+        print(f"Fome {self.fome}, Felicidade {self.felicidade}")
+
+
